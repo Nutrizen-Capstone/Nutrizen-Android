a4dd946f97b610ad6d8a1ca467933b0408c7272f
alfizu <alfiziaulhaq@gmail.com>
2023-12-12 17:17:29 +0700

    complete personal data form

--- app/src/main/java/com/capstone/nutrizen/ui/components/dropdown.kt
+++ app/src/main/java/com/capstone/nutrizen/ui/components/dropdown.kt
@@ -21,6 +21,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun dropdown(
     id : (value: Int) -> Unit
